87a53e3352bdefeda207de5385bada1574417fbf
Andrew M Olney <aolney@memphis.edu>
2017-05-06 15:11:26 -0500

    took material ui back to 0.6.x

--- samples/material-ui/src/main.fsx
+++ samples/material-ui/src/main.fsx
@@ -1,12 +1,12 @@
-module Main
+
+// Load Fable.Core and bindings to JS global objects
+#r "../node_modules/fable-core/Fable.Core.dll"
+#load "../node_modules/fable-import-electron/Fable.Import.Electron.fs"
 
 open Fable.Core
 open Fable.Core.JsInterop
 open Fable.Import
 open Fable.Import.Electron
-
-// Convinience operator to ignore results
-let inline (!>) x = ignore x
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -22,34 +22,34 @@ let createMainWindow () =
     window.loadURL("file://" + Node.__dirname + "/../index.html");
 
     #if DEBUG
-    !>Node.fs.watch(Node.__dirname + "/renderer.js", fun _ ->
-        window.webContents.reloadIgnoringCache()
-    )
+    Node.fs.watch(Node.__dirname + "/renderer.js", fun _ ->
+        window.webContents.reloadIgnoringCache() |> ignore
+    ) |> ignore
     #endif
 
     // Emitted when the window is closed.
-    !>window.on("closed", !!(fun () ->
+    window.on("closed", unbox(fun () ->
         // Dereference the window object, usually you would store windows
         // in an array if your app supports multi windows, this is the time
         // when you should delete the corresponding element.
         mainWindow <- Option.None
-    ))
+    )) |> ignore
 
     mainWindow <- Some window
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
-!>electron.app.on("ready", !!createMainWindow)
+electron.app.on("ready", unbox createMainWindow)
 
 // Quit when all windows are closed.
-!>electron.app.on("window-all-closed", !!(fun () ->
+electron.app.on("window-all-closed", unbox(fun () ->
     // On OS X it is common for applications and their menu bar
     // to stay active until the user quits explicitly with Cmd + Q
     if Node.``process``.platform <> "darwin" then
         electron.app.quit()
 ))
 
-!>electron.app.on("activate", !!(fun () ->
+electron.app.on("activate", unbox(fun () ->
     // On OS X it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
     if mainWindow.IsNone then
--- samples/material-ui/webpack.config.js
+++ samples/material-ui/webpack.config.js
@@ -1,57 +1,27 @@
-var path = require("path");
-var webpack = require("webpack");
-
-function resolve(filePath) {
-  return path.join(__dirname, filePath)
-}
-
-var babelOptions = {
-  presets: [["es2015", { "modules": false }]],
-  plugins: ["transform-runtime"]
-}
-
-var isProduction = process.argv.indexOf("-p") >= 0;
-console.log("Bundling for " + (isProduction ? "production" : "development") + "...");
-
 module.exports = {
-  devtool: "source-map",
   entry: {
-    main: resolve("./src/Main/Main.fsproj"),
-    renderer: resolve("./src/Renderer/renderer.fsproj")
+    main: "./temp/main",
+    renderer: "./temp/renderer"
   },
   output: {
     filename: "[name].js",
-    path: resolve("./app/js"),
+    path: "./app/js",
     libraryTarget: "commonjs2"
   },
   externals: {
     electron: true
   },
-  target: "node",  
+  target: "node",
   node: {
     __dirname: false,
     __filename: false
   },
+  devtool: "source-map",
   module: {
-    rules: [
-      {
-        test: /\.fs(x|proj)?$/,
-        use: {
-          loader: "fable-loader",
-          options: {
-            babel: babelOptions,
-            define: isProduction ? [] : ["DEBUG"]
-          }
-        }
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules[\\\/](?!fable-)/,
-        use: {
-          loader: 'babel-loader',
-          options: babelOptions
-        },
-      },
-    ]
+    preLoaders: [{
+      loader: "source-map-loader",
+      exclude: /node_modules/,
+      test: /\.js$/
+    }]
   }
 };
